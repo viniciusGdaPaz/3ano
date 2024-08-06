619a5afb3c50dd39ec8884538edae31263e444fe
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-06 12:00:27 -0300

    Create get.php

---
+++ Linguagem para web/aula6/get.php
@@ -0,0 +1,23 @@
+<?php  /**$pais = $_GET["pais"];
+
+$ouro = $_GET["ouro"];
+
+$bronze = $_GET"bronze"];
+
+$prata = $_GET["prata"];
+
+
+
+
+
+
+echo"Pais:".$pais . "<br> ouro:". $ouro . " <br> prata:" .$prata . " <br> bronze:" .$bronze ;
+ */
+
+
+ $n1 = $_GET["n1"];
+ $n2 = $_GET["n2"];
+
+ echo"numero 1 :". $n1 . " <br> numero 2 :".$n2 . "<br> soma:". ($n1+$n2);
+
+?>
