e6c3f5a19143fa54771f776bffb6b21c3d970947
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-06 12:00:43 -0300

    Create post.php

---
+++ Linguagem para web/aula6/post.php
@@ -0,0 +1,23 @@
+<?php  /**$pais = $_POST["pais"];
+
+$ouro = $_POST["ouro"];
+
+$bronze = $_POST["bronze"];
+
+$prata = $_POST["prata"];
+
+
+
+
+
+
+echo"Pais:".$pais . "<br> ouro:". $ouro . " <br> prata:" .$prata . " <br> bronze:" .$bronze ;
+ */
+
+
+ $n1 = $_POST["n1"];
+ $n2 =  $_POST["n2"];
+
+ echo"numero 1 :". $n1 . " \n numero 2 :".$n2 . " \nsoma:". ($n1+$n2);
+
+?>
