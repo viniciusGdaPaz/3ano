5fec4123a260879bb1faefbb0be7a5fca38191a4
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-12 20:21:51 -0300

    Update ex1.php

--- Linguagem para web/aula6/exercicio/ex1.php
+++ Linguagem para web/aula6/exercicio/ex1.php
@@ -19,8 +19,81 @@ if($tipo == "A"||$tipo == "a"){
 
 }else if( $tipo == "C"||$tipo == "c"){
 
+    Class Pessoa{
+      private $nomeP;
+      private $sobrenomeP;
+      private $idadeP;
+
+      public function __construct($n = "", $s="",$i = "")
+      {
+        $this->nomeP = $n;
+        $this->sobrenomeP = $s;
+        $this->idadeP = $i;
+      }
+
+      /**
+       * Get the value of nomeP
+       */
+      public function getNomeP()
+      {
+            return $this->nomeP;
+      }
+
+      /**
+       * Set the value of nomeP
+       */
+      public function setNomeP($nomeP): self
+      {
+            $this->nomeP = $nomeP;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of sobrenomeP
+       */
+      public function getSobrenomeP()
+      {
+            return $this->sobrenomeP;
+      }
+
+      /**
+       * Set the value of sobrenomeP
+       */
+      public function setSobrenomeP($sobrenomeP): self
+      {
+            $this->sobrenomeP = $sobrenomeP;
+
+            return $this;
+      }
+
+      /**
+       * Get the value of idadeP
+       */
+      public function getIdadeP()
+      {
+            return $this->idadeP;
+      }
+
+      /**
+       * Set the value of idadeP
+       */
+      public function setIdadeP($idadeP): self
+      {
+            $this->idadeP = $idadeP;
+
+            return $this;
+      }
+    }
+    $pessoa = new Pessoa($nome,$sobrenome,$idade);
+
+    echo"Nome completo: ". $pessoa->getNomeP() . " " . $pessoa->getSobrenomeP();
+    echo"Idade: " . $pessoa->getIdadeP();
+
 }else{
-    echo"";
+    echo"ERRO, INFORMADO PARAMETRO DIFERENTE DE A E C";
+
+      
 }
 
 
