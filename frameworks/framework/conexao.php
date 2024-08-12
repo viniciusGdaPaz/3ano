385dddee356a2613d7f39a365a629f6d21090129
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-12 11:33:44 -0300

    Update conexao.php

--- frameworks/framework/conexao.php
+++ frameworks/framework/conexao.php
@@ -1,6 +1,9 @@
 <?php
- $conn = new PDO("msql:host=localhost; dbname=framework" , "root", "bancodedados");
-    
- 
-
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+try{
+   $conn= new PDO("mysql:host=localhost;dbname=framework","root","bancodedados");
+}catch(PDOException $e){
+	echo "erro".$e->getMessage();
+}
 ?>
