d80ad7e33e15a0e68597e9855443402e7ae414f4
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-13 10:51:03 -0300

    Update ex2.php

--- Linguagem para web/aula6/exercicio/ex2.php
+++ Linguagem para web/aula6/exercicio/ex2.php
@@ -2,48 +2,52 @@
 
 $n1 = $_GET["numero1"];
 $n2 = $_GET["numero2"];
-$equacao =  $_GET["equacao"];
+/**$equacao =  $_GET["equacao"];*/
 
 echo"informe os valores 1 e 2";
-echo"opções de equção :";
+/**echo" <br>opções de equção :";
 echo"<br> soma:1 ";
 echo"<br> subtração:2 ";
 echo"<br> divisão:3 ";
 echo"<br> multiplicação:4 ";
-echo"<br> resto:5 ";
+echo"<br> resto:5 <br>";*/
 
 
 if( $n1 == null ){
-    echo" valor numero1 não foi informado";
+    echo" <br>valor numero1 não foi informado";
 }
 else if( $n2 == null ){
-    echo" valor numero2 não foi informado";
+    echo"<br> valor numero2 não foi informado";
 }
-else{
+else if ($n1 != null & $n2 !=null){
 
 
 
-if($equacao == 1){
+/**if($equacao == 1){
 
-    echo"soma dos valores numero 1 e numero 2 :".$n1 + $n2;
+    echo"soma dos valores numero 1 e numero 2 : ".$n1 + $n2;
 
 }else if($equacao == 2){
-    echo"subtração dos valores numero 1 e numero 2 :".$n1 - $n2;
+    echo"subtração dos valores numero 1 e numero 2 : ".$n1 - $n2;
 
 }else if($equacao == 3){
-    echo"divisão dos valores numero 1 pelo numero 2 :".$n1 / $n2;
+    echo"divisão dos valores numero 1 pelo numero 2 : ".$n1 / $n2;
 
 }else if($equacao == 4){
-    echo"multiplicaçõa dos valores numero 1 pelo numero 2 :".$n1 * $n2;
+    echo"multiplicaçõa dos valores numero 1 pelo numero 2 : ".$n1 * $n2;
 
 }else if($equacao == 5){
-    echo"resto dos valores numero 1 dividido pelo numero 2 :".$n1 % $n2;
+    echo"resto dos valores numero 1 dividido pelo numero 2 : ".$n1 % $n2;
 
 }else if ($equacao == null){
     echo"<br>valor  informado invalido";
-}
-
-
+}*/
+echo"<br>numero 1 = ".$n1."      numero 2 = ".$n2     ;
+echo"<br>soma dos valores numero 1 e numero 2 : ".$n1 + $n2;
+echo"<br>subtração dos valores numero 1 e numero 2 : ".$n1 - $n2;
+echo"<br>divisão dos valores numero 1 pelo numero 2 : ".$n1 / $n2;
+echo"<br>multiplicaçõa dos valores numero 1 pelo numero 2 : ".$n1 * $n2;
+echo"<br>resto dos valores numero 1 dividido pelo numero 2 : ".$n1 % $n2;
 }
 
 
