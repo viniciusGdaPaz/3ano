bda40674efa8fb01389f3c6dccedf5ba455060a9
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-19 08:56:35 -0300

    Create form_exp.php

---
+++ Linguagem para web/aula7/form_exp.php
@@ -0,0 +1,12 @@
+<?php
+         $nome = $_POST["nome"];
+         $nivelEnsino = $_POST["nivelEnsino"];
+
+
+         echo"Seu nome é:".$nome."   nivel de Ensino:".$nivelEnsino;
+
+         echo " <br><a href='exp.php'>Informar outra pessoa</a>";
+    
+
+
+?>
