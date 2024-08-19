63d79bd31716bd6adb2dd0eeab6c590543e844f1
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-19 09:07:20 -0300

    Create cor.php

---
+++ Linguagem para web/aula7/cor.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cor</title>
+</head>
+<body>
+    <h1>Informe a cor</h1>
+
+    <form action="" method="post">
+
+        <label for="cor"> Informe a Cor:</label>
+        <select name="cor">
+        <option value="">----Selecione a cor----</option>
+        <option value="Tomato">Tomato</option>
+        <option value="Orange">Orange</option>
+        <option value="DodgerBlue">DodgerBlue</option>
+        <option value="MediumSeaGreen">MediumSeaGreen</option>
+        <option value="Gray">Gray</option>
+        <option value="SlateBlue">SlateBlue</option>
+        <option value="DodgerBlue">DodgerBlue</option>
+        <option value="MediumSeaGreen">MediumSeaGreen</option>
+
+
+      </select>
+
+
+      <input type="submit" value="Enviar">
+
+
+
+
+
+    </form>
+</body>
+</html>
