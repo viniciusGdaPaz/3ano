af0a4b37863e19d6cfda1b30471b244421b42719
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-19 11:50:11 -0300

    Update conexao.php

--- frameworks/framework/conexao.php
+++ frameworks/framework/conexao.php
@@ -1,9 +1,26 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-try{
-   $conn= new PDO("mysql:host=localhost;dbname=framework","root","bancodedados");
-}catch(PDOException $e){
-	echo "erro".$e->getMessage();
-}
+    try{
+    /** $usuario = "root";
+    $senha = "bancodedados";  */    
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+    $banco = $_POST['banco'];
+    $servidor = $_POST['servidor'];
+
+
+
+    $conn = new PDO("mysql:host=".$servidor.";dbname=$banco","$usuario","$senha");
+    /**echo"<br>Conexão estabelecida com sucesso";
+     echo"<br>Servidor: " . $_POST['servidor'];
+     echo"<br>Banco: ".$_POST['banco'];
+     echo"<br>Usuario: ".$_POST['usuario'];
+     echo"<br>Senha: ".$_POST['senha']; */
+     header("Location:frameworkMVC.php?msg=0");
+    }
+    catch(PDOException $e){
+    
+    /**echo "Falha na conexão com o banco de dados";
+    echo "ERRO: $e->getMessage()";
+ */    header("Location:frameworkMVC.php?msg=1");
+    }
 ?>
