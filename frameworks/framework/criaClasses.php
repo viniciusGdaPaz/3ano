314272c28a9666c7307812771446cac3ced98259
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-08-26 11:07:26 -0300

    Update criaClasses.php

--- frameworks/framework/criaClasses.php
+++ frameworks/framework/criaClasses.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 class criaClasses
     {
-    static function criar($conn,$banco)
+    static function criar($conn,$banco, $usuario, $senha)
     {
        $banco="Tables_in_".$banco;
        $query = $conn->query("show tables");
@@ -22,7 +22,7 @@ class criaClasses
             mkdir("sistema/dao");
         file_put_contents("sistema/index.php", "");
         foreach ($tabelas as $tabela) {
-            $conteudo .= "class " . ucfirst($tabela->$banco) . "{\n";
+            $conteudo .= "<?php\nclass " . ucfirst($tabela->$banco) . "{\n";
             $queryAttr = $conn->query("show columns from " . $tabela->$banco);
             $atributos = $queryAttr->fetchAll(PDO::FETCH_OBJ);
             foreach ($atributos as $atributo) {
@@ -33,23 +33,30 @@ class criaClasses
                 $conteudo .= "function get" . ucfirst($atributo->Field) . "(){
               return $" . "this->" . $atributo->Field . ";\n}\n";
                 $conteudo .= "function set" . ucfirst($atributo->Field) . "($" . $atributo->Field . "){
-             $" . "this->" . $atributo->Field . "=" . $atributo->Field . ";
+             $" . "this->" . $atributo->Field . "=$" . $atributo->Field . ";
          }\n";
             }
+            $conteudo .="}\n?>";
             file_put_contents("sistema/model/" . $tabela->$banco . ".php", $conteudo);
             $conteudo = "";
         }
          //Criação de classes da camada DAO
          foreach ($tabelas as $tabela) {
-             $conteudo = "class " . ucfirst($tabela->$banco) . "DAO{\n";
+             $conteudo = "<?php\nclass " . ucfirst($tabela->$banco) . "DAO{\n";
              $conteudo .= "\tpublic __construct() {\n}\n";
              $conteudo .= "\tfunction inserir() {\n}\n";
              $conteudo .= "\tfunction excluir() {\n}\n";
              $conteudo .= "\tfunction buscar() {\n}\n";
-             $conteudo .= "\tfunction alterar() {\n}\n}";
+             $conteudo .= "\tfunction alterar() {\n}\n}?>";
              file_put_contents("sistema/dao/" . $tabela->$banco . "DAO.php", $conteudo);
              $conteudo = "";
          }
+         //Criação do arquivo de conexão
+        $conteudo ="<?php\nclass Conexao{\n";
+         
+        $conteudo .= "}\n?>";    
+         file_put_contents("sistema/dao/" . "conexao.php" . "DAO.php", $conteudo);
+
          //Criação de Classes da camada control
          $conteudo = "";
          foreach($tabelas as $tabela){
