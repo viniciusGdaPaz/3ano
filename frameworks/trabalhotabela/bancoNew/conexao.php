a78bc06796a854e8ae08ceee63dbb2f8aa5024c1
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-09-02 11:44:51 -0300

    Create conexao.php

---
+++ frameworks/trabalhotabela/bancoNew/conexao.php
@@ -0,0 +1,17 @@
+<?php
+include "criaClasses.php";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+try{
+    $server=$_POST["servidor"];
+    $banco=$_POST["banco"];
+    $usuario=$_POST["usuario"];
+    $senha=$_POST["senha"];
+    $conn= new PDO("mysql:host=".$server.";dbname=$banco","$usuario","$senha");
+    criaClasses::criar($conn,$banco,$usuario,$senha);
+    header("Location:criarFormulario.php?bd=".$banco);
+ }catch(PDOException $e){
+	echo "erro".$e->getMessage();
+    header("Location:frameworkMVC.php?msg=1");
+}
+?>
