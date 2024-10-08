1bea169d20b84ec675d23efe86760720cc2997e1
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-10-08 11:54:28 -0300

    Update AlunoDao.php

--- Linguagem para web/AULA10/crud_alunos/dao/AlunoDao.php
+++ Linguagem para web/AULA10/crud_alunos/dao/AlunoDao.php
@@ -2,6 +2,8 @@
  
 
  include_once(__DIR__."/../util/Connection.php");
+ include_once(__DIR__."/../model/Aluno.php");
+ include_once(__DIR__."/../model/Curso.php");
  class AlunoDao{
 
     private $conn;
@@ -17,7 +19,30 @@
         $stm->execute();
         $result = $stm->fetchAll();
 
-        return $result;
+        $alunos = $this->mapAlunos($result);
+        return $alunos;
+    }
+
+   
+    private function mapAlunos(array $result){
+
+        $alunos = array();
+
+        foreach($result as $reg){
+            $aluno = new Aluno();
+            $aluno->setId($reg["id"]);
+            $aluno->setIdade($reg["idade"]);
+            $aluno->setNome($reg["nome"]);
+            $aluno->setEstrangeiro($reg["estrangeiro"]);
+
+            $curso = new Curso();
+            $curso->setId($reg['id_curso']);
+            $aluno->setCurso($curso);
+
+            array_push($alunos, $aluno);
+        }
+
+        return $alunos;
     }
  }
 ?>
