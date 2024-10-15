2b0af77d812174330d4443f153d29e3a89bbd41c
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-10-15 12:00:47 -0300

    Create CursoDao.php

---
+++ Linguagem para web/AULA10/crud_alunos/dao/CursoDao.php
@@ -0,0 +1,38 @@
+<?php
+include_once(__DIR__."/../util/Connection.php");
+include_once(__DIR__."/../model/Curso.php");
+
+class CursoDao{
+
+    private $conn;
+
+    public function __construct(){
+        $this->conn = Connection::getConnection();
+    }
+
+    public function list(){
+        $sql = "SELECT * FROM  cursos";
+        $stm = $this->conn->prepare($sql);
+        $stm->execute();
+        $result = $stm->fetchAll();
+        $cs = $this->mapCursos($result);
+        return $cs;
+    }
+
+    public function mapCursos(array $result){
+        $cursos = array();
+
+        foreach($result as $reg){
+            $curso = new Curso();
+            $curso->setId($reg['id']);
+            $curso->setNome($reg['nome']);
+            $curso->setTurno($reg['turno']);
+
+            array_push($cursos, $curso);
+        }
+
+        return $cursos;
+    }
+}
+
+?>
