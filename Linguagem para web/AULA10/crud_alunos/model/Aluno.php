011742916188a2ad0d8f7b26f6a9291ea233f2aa
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-10-22 12:04:07 -0300

    Update Aluno.php

--- Linguagem para web/AULA10/crud_alunos/model/Aluno.php
+++ Linguagem para web/AULA10/crud_alunos/model/Aluno.php
@@ -2,11 +2,11 @@
 
 include_once(__DIR__."/Curso.php");
 class Aluno{
-    private int $id;
-    private string $nome;
-    private int $idade;
-    private string $estrangeiro;
-    private Curso $curso;
+    private ?int $id;
+    private ?string $nome;
+    private ?int $idade;
+    private ?string $estrangeiro;
+    private ?Curso $curso;
 
 
 
@@ -14,67 +14,7 @@ class Aluno{
     
 
 
-    /**
-     * Get the value of id
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set the value of id
-     */
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of nome
-     */
-    public function getNome(): string
-    {
-        return $this->nome;
-    }
-
-    /**
-     * Set the value of nome
-     */
-    public function setNome(string $nome): self
-    {
-        $this->nome = $nome;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of idade
-     */
-    public function getIdade(): int
-    {
-        return $this->idade;
-    }
-
-    /**
-     * Set the value of idade
-     */
-    public function setIdade(int $idade): self
-    {
-        $this->idade = $idade;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of estrangeiro
-     */
-    public function getEstrangeiro(): string
-    {
-        return $this->estrangeiro;
-    }
+  
 
     public function getEstrangeiroText(){
 
@@ -93,23 +33,84 @@ class Aluno{
         
     }
 
+
+
+    /**
+     * Get the value of id
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(?string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of idade
+     */
+    public function getIdade(): ?int
+    {
+        return $this->idade;
+    }
+
+    /**
+     * Set the value of idade
+     */
+    public function setIdade(?int $idade): self
+    {
+        $this->idade = $idade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of estrangeiro
+     */
+    public function getEstrangeiro(): ?string
+    {
+        return $this->estrangeiro;
+    }
+
     /**
      * Set the value of estrangeiro
      */
-    public function setEstrangeiro(string $estrangeiro): self
+    public function setEstrangeiro(?string $estrangeiro): self
     {
         $this->estrangeiro = $estrangeiro;
 
         return $this;
     }
 
-    
-
-    
     /**
      * Get the value of curso
      */
-    public function getCurso(): Curso
+    public function getCurso(): ?Curso
     {
         return $this->curso;
     }
@@ -117,7 +118,7 @@ class Aluno{
     /**
      * Set the value of curso
      */
-    public function setCurso(Curso $curso): self
+    public function setCurso(?Curso $curso): self
     {
         $this->curso = $curso;
 
