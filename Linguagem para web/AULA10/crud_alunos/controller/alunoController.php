53b409ac6b51ac8a0fbb82518a18595a4195d70a
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-10-22 12:03:05 -0300

    Update alunoController.php

--- Linguagem para web/AULA10/crud_alunos/controller/alunoController.php
+++ Linguagem para web/AULA10/crud_alunos/controller/alunoController.php
@@ -10,6 +10,16 @@ class AlunoController{
         $alunos = $alunoDao->list();
         return $alunos;
     }
+
+    public function inserir($aluno){
+     $alunoDao = new AlunoDao();
+     $alunoDao->insert($aluno);  
+    }
+
+    public function deletar($id){
+       $alunoDao = new AlunoDao();
+       $alunoDao->delete($id);
+    }
 }
 
 ?>
