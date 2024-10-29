5ea4c3cf09c3d9c1918a0a065962e55e7f478c21
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-10-29 11:55:56 -0300

    Update inserir.php

--- Linguagem para web/AULA10/crud_alunos/view/alunos/inserir.php
+++ Linguagem para web/AULA10/crud_alunos/view/alunos/inserir.php
@@ -3,6 +3,8 @@ include_once(__DIR__."/../../model/Aluno.php");
 
 include_once(__DIR__."/../../model/Curso.php");
 include_once(__DIR__."/../../controller/AlunoController.php");
+$msgErro = "";
+$aluno = null;
 if(isset($_POST['nome'])){
     
     $nome = trim($_POST['nome'])? trim($_POST['nome']):null;
@@ -17,16 +19,28 @@ if(isset($_POST['nome'])){
     
     $aluno->setEstrangeiro($estrang);
     
-    $cursoObj = new Curso();
-    $cursoObj->setId($curso);
-    $aluno->setCurso($cursoObj);
-
+    if($curso){
+        
+        $cursoObj = new Curso();
+        $cursoObj->setId($curso);
+        $aluno->setCurso($cursoObj);
+    }else{
+        $aluno->setCurso(null);
+    }
+    
     $alunoCont = new AlunoController();
-    $alunoCont->inserir($aluno);
-    //print_r($aluno);
-    header("location: listar.php");
-}else
-echo"Usuario ainda não informou os dados";
+    $erros=$alunoCont->inserir($aluno);
+    if(count($erros)<= 0){
+        
+        header("location: listar.php");
+        
+        
+    }else{
+        $msgErro = implode("<br>",$erros);
+        //echo "<p style='color: red;'>".implode("<br>",$erros)."</p>";
+    }
+    
+}
 
 include("form.php");
 ?>
