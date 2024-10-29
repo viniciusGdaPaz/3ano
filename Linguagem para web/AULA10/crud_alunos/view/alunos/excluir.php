8389fb4b0cb09ca9ac04090b87ccaf84b2a644fa
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-10-29 11:55:13 -0300

    Update excluir.php

--- Linguagem para web/AULA10/crud_alunos/view/alunos/excluir.php
+++ Linguagem para web/AULA10/crud_alunos/view/alunos/excluir.php
@@ -1,18 +1,20 @@
 <?php
+#Página para excluir um aluno
 
-include_once(__DIR__."/../../controller/AlunoController.php");
+require_once(__DIR__ . "/../../controller/AlunoController.php");
 
+//1- Capturar o ID do aluno utilizando a superglobal $_GET
+$id = isset($_GET['id']) && is_numeric($_GET['id']) ? trim($_GET['id']) : 0;
 
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-
+if($id) {
+    //2- Chamar o AlunoController para excluir o aluno pelo ID 
+    //Excluir
     $alunoCont = new AlunoController();
-    $alunoCont->deletar($id);
+    $erros = $alunoCont->deletar($id);
+    
+    //3- Voltar para o listar.php
     header("location: listar.php");
-}else{
-    echo"ID não informado";
+} else {
+    echo "Aluno não encontrado!<br>";
+    echo "<a href='listar.php'>Voltar</a>";
 }
-
-
-
-?>
