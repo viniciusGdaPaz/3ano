8d5981191af5415b588cdb1b5cc38d322dd73ed1
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-11-24 02:29:33 -0300

    Create config.php

---
+++ Linguagem para web/AULA10/crud_alunos/trabalho/util/config.php
@@ -0,0 +1,11 @@
+<?php
+
+//Mostrar erros do PHP
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Configurar essas variáveis de acordo com o seu ambiente
+define("DB_HOST", "localhost");
+define("DB_NAME", "db_rpg");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
