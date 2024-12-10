87d0929f5f6cb690674db608cad787fd43ec3130
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-12-10 11:39:43 -0300

    Create Curso.php

---
+++ Linguagem para web/SESSION/crud_alunos_login/model/Curso.php
@@ -0,0 +1,80 @@
+<?php
+
+class Curso {
+
+    private int $id;
+    private string $nome;
+    private string $turno;
+
+    public function __toString() {
+       return $this->nome . " - " . $this->turno;
+    }
+
+    /**
+     * Get the value of id
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of turno
+     */
+    public function getTurno(): string
+    {
+        return $this->turno;
+    }
+
+    public function getTurnoTexto(): string
+    {
+        if($this->turno == "M")
+            return "Matutino";
+
+        if($this->turno == "V")
+            return "Vespertino";
+
+        if($this->turno == "N")
+            return "Noturno";
+
+        return "";
+    }
+
+    /**
+     * Set the value of turno
+     */
+    public function setTurno(string $turno): self
+    {
+        $this->turno = $turno;
+
+        return $this;
+    }
+}
