ca3e7847230b53f8130d0abab1b8ce19ea288f59
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-12-10 11:36:59 -0300

    Create UsuarioController.php

---
+++ Linguagem para web/SESSION/crud_alunos_login/controller/UsuarioController.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once(__DIR__ . "/../dao/UsuarioDao.php");
+require_once(__DIR__ . "/../service/UsuarioService.php");
+
+class UsuarioController {
+
+    private UsuarioDAO $usuarioDAO;
+    private UsuarioService $usuarioService;
+
+    public function __construct() {
+        $this->usuarioDAO = new UsuarioDAO();    
+        $this->usuarioService = new UsuarioService();        
+    }
+
+    public function logar(string $login, string $senha) {
+        $erros = $this->usuarioService->validarDadosLogin($login, $senha);
+
+        if(! empty($erros))
+            return $erros;
+
+        //TO DO - Implementar validação de login
+        $usuario = $this->usuarioDAO->findByLoginSenha($login, $senha);
+        if(!$usuario){
+            return(array("Login ou senha invalidos!"));
+        }
+
+        $this->usuarioService->salvarSessao($usuario);
+        
+        //Retorna um array vazio para indicar que tudo deu certo
+        return array();
+    }
+
+}
