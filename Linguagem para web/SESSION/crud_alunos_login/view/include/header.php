0532a51b06d98814461d8ca93c9da69c503e89ce
viniciusGdaPaz <116355808+viniciusGdaPaz@users.noreply.github.com>
2024-12-17 11:29:19 -0300

    Create header.php

---
+++ Linguagem para web/SESSION/crud_alunos_login/view/include/header.php
@@ -0,0 +1,17 @@
+<?php
+include_once(__DIR__."/../../util/config.php")
+?>
+<!doctype html>
+<html lang="pt">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Crud alunos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <!-- Container bootstrap -->
+    <div class="container">
